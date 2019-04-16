cdfc78a780b0bfa69a72f6d63e17dfa376dcdc27
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2019-04-16 14:57:37 +0100

    feature(sign-in-endpoint): create sign-in endpoint [Start #165329420]

--- API/controllers/user.controller.js
+++ API/controllers/user.controller.js
@@ -3,15 +3,16 @@ import userData from '../utils/data';
 class userController {
   static signIn(req, res) {
     const { email, password } = req.body;
-    const user = userData.find(u => u[0].email === email && u[0].password === password);
-    if (user === null) {
+    const userA = userData.filter(user => user.email === email && user.password === password);
+    if (!userA) {
       return res.json({
+        status: 404,
         message: 'no user found',
       }).status(404);
     }
     return res.json({
       message: 'success',
-      data: user,
+      data: userA,
     }).status(200);
   }
 }
