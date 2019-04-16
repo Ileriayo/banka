03d7821cd10c7fd4a572d446fbd43b0922096747
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2019-04-16 13:40:07 +0100

    update eslint rules

---
+++ API/utils/data.js
@@ -0,0 +1,40 @@
+export default {
+  user: [
+    {
+      id: 1,
+      email: 'ileriayo@gmail.com',
+      firstName: 'Ileriayo',
+      lastName: 'Adebiyi',
+      password: 'password',
+      type: 'client',
+      isAdmin: false,
+    },
+    {
+      id: 2,
+      email: 'dave@gmail.com',
+      firstName: 'Dave',
+      lastName: 'Dave',
+      password: 'password',
+      type: 'staff',
+      isAdmin: false,
+    },
+    {
+      id: 3,
+      email: 'joe@gmail.com',
+      firstName: 'Joe',
+      lastName: 'Joe',
+      password: 'password',
+      type: 'client',
+      isAdmin: false,
+    },
+    {
+      id: 4,
+      email: 'Dan@gmail.com',
+      firstName: 'Dan',
+      lastName: 'Dan',
+      password: 'password',
+      type: 'staff',
+      isAdmin: true,
+    },
+  ],
+};
