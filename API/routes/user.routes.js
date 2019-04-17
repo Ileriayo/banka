ad0ac26b885b32ad2c2f2d1a663a83fc1fb22b7e
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2019-04-17 03:52:10 +0100

    feature(sign-up-endpoint): create sign up endpoint [Start #165329418]

--- API/routes/user.routes.js
+++ API/routes/user.routes.js
@@ -5,5 +5,6 @@ import userController from '../controllers/user.controller';
 const userRouter = express.Router();
 
 userRouter.post('/login', userController.signIn);
+userRouter.post('/signup', userController.signUp);
 
 export default userRouter;
